1800d48e6083b77896c8a75fdda2803ef46d3580
Marecare33 <marko.tomic4321@gmail.com>
2024-08-16 00:05:27 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your submission!');
+    // Here you would typically send the form data to a server
+});
